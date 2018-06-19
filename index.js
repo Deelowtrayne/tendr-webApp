619f816801c877c48006302672ccc1304b787b02
Luvuyo Sono <luvuyosono@gmail.com>
2018-06-19 23:42:13 +0200

    Project setup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//
+// imports
 const express = require('express');
 const bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
@@ -16,13 +16,30 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+// HOME PAGE
 app.get("/", function (req, res) {
     res.render('home');
 })
 
+// SEARCH RESULTS
+app.get("/search", function (req, res) {
+    res.render('search');
+})
+
 app.post("/search", function (req, res) {
     var q = req.body.searchStr;
-    res.send();
+    res.render('search');
+})
+
+// TENDER VIEW
+app.get('/tender/:tenderNumber', function(req, res){
+    var tenderNum = req.params.tenderNumber;
+    res.send(`
+        <h2>Viewing tender number: ${tenderNum}</h2>
+        <p>Nothing here yet</p>
+    `)
+    //res.render('tender');
 })
 
 app.listen(5000, function () {
