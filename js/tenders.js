5e63b487b2f8440a84898ba7be8ac8ee79bf2b08
banelengwane <banelengwane@gmail.com>
2018-06-21 11:38:50 +0200

    added a filterByValue function

--- js/tenders.js
+++ js/tenders.js
@@ -1,9 +1,24 @@
-function Tenders(stored) {
-    function filterByIndustry(dep) {
-        
-    }
-    
-    return {
+module.exports = function(stored) {
+    var data = [];
 
+    if (stored) {
+      for (let i = 0; i < stored.length; i++){
+        data.push(stored[i]);
+      }
+    }
+
+    function filterByValue(minValue, maxValue){
+      var list = [];
+      for(var i = 0; i < data.length; i++){
+        if(data[i].value > minValue && data[i].value < maxValue ){
+          list.push(data[i]);
+        }
+      }
+
+      return list;
+    }
+    return {
+      data,
+      filterByValue
     }
 }
